8086c1e16e8b214459b5015f33d73e0ec8048500
peterfei <peterfeispace@gmail.com>
2014-10-03 15:48:55 +0800

    用户登陆界面

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Admin::SessionsController < ApplicationController
+  layout 'signinout'
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-  end
-
-  get 'users/new'
-
-  namespace :admin do
     resources :dashboards
+    resources :sessions, only: [:new, :create, :destroy]
   end
 
+  #get 'users/new'
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
